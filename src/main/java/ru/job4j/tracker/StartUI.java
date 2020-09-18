a1e2f8cf310f20304e4f06553f49dbb1d8e45b35
reybos <andreybosiy@yandex.ru>
2020-09-18 10:34:09 +0300

    6. Date. Отображение даты. [#352776]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item item = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String itemCreatedDateFormat = item.getCreated().format(formatter);
+        System.out.println(itemCreatedDateFormat);
+    }
+}
