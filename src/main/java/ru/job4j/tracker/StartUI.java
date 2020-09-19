3b4048e23dce152474848fcafe5f99eecb4bf920
reybos <andreybosiy@yandex.ru>
2020-09-19 13:49:36 +0300

    5. Tracker - хранилище [#352795]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,13 +1,27 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
     public static void main(String[] args) {
+        Tracker tracker = new Tracker();
         Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String itemCreatedDateFormat = item.getCreated().format(formatter);
-        System.out.println(itemCreatedDateFormat);
-        System.out.println(item);
+        tracker.add(item);
+        System.out.println(tracker.findById(1));
+        for (int i = 0; i < 5; i++) {
+            item = new Item();
+            item.setName("name" + i);
+            tracker.add(item);
+        }
+        Item repeatItem = new Item();
+        repeatItem.setName("name1");
+        tracker.add(repeatItem);
+        Item[] repeatList = tracker.findByName("name1");
+        for (int i = 0; i < repeatList.length; i++) {
+            System.out.println(repeatList[i]);
+        }
+        System.out.println();
+        Item[] allList = tracker.findAll();
+        for (int i = 0; i < allList.length; i++) {
+            System.out.println(allList[i]);
+        }
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,21 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int countEquals = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item.getName() != null && item.getName().equals(key)) {
+                rsl[countEquals++] = item;
+            }
+        }
+        return Arrays.copyOf(rsl, countEquals);
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
     }
 }
