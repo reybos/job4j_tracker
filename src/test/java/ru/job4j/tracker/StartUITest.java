b60b79c2acafb68cb1a21fc91b3094ed89bbeb6a
reybos <andreybosiy@yandex.ru>
2020-09-25 16:35:55 +0300

    1.1. Тесты на StartUI. [#352740]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -199,4 +199,26 @@ public class StartUITest {
                         "1. Exit Program" + System.lineSeparator()
         ));
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                                + "0. Exit Program%n"
+                                + "Wrong input, you can select: 0 .. 0%n"
+                                + "Menu.%n"
+                                + "0. Exit Program%n"
+                )
+        ));
+    }
 }
