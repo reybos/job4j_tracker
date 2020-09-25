0a38e379e709442ea26e715e4dd625bb45210770
reybos <andreybosiy@yandex.ru>
2020-09-25 16:22:52 +0300

    1. Обеспечить бесперебойную работу приложения Tracker [#352742]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -12,6 +12,10 @@ public class StartUI {
         while (run) {
             this.showMenu(actions);
             int select = input.askInt("Select: ");
+            if (select < 0 || select >= actions.length) {
+                out.println("Wrong input, you can select: 0 .. " + (actions.length - 1));
+                continue;
+            }
             run = actions[select].execute(input, tracker);
         }
     }
@@ -25,7 +29,7 @@ public class StartUI {
 
     public static void main(String[] args) {
         Output out = new ConsoleOutput();
-        Input input = new ConsoleInput();
+        Input input = new ValidateInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new CreateAction(out),
