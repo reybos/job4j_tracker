adee4d871e8e3bf556641c91cf6174265d1e2392
reybos <andreybosiy@yandex.ru>
2020-10-09 08:51:50 +0300

    3. Банковские переводы [#352704]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -52,7 +52,7 @@ public class BankService {
         boolean rsl = false;
         Account srcAccount = findByRequisite(srcPassport, srcRequisite);
         Account destAccount = findByRequisite(destPassport, destRequisite);
-        if (srcAccount != null && destAccount != null && (srcAccount.getBalance() - amount) == 0) {
+        if (srcAccount != null && destAccount != null && (srcAccount.getBalance() - amount) >= 0) {
             srcAccount.setBalance(srcAccount.getBalance() - amount);
             destAccount.setBalance(destAccount.getBalance() + amount);
             rsl = true;
