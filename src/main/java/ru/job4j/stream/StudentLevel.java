805cfe2384a65b618e29cc16d85314cb0d504546
reybos <andreybosiy@yandex.ru>
2020-10-31 17:59:27 +0300

    0. Stream API улучшения [#352540]

---
+++ src/main/java/ru/job4j/stream/StudentLevel.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StudentLevel {
+    public static List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream()
+                .flatMap(Stream::ofNullable)
+                .sorted((left, right) -> Integer.compare(right.getScore(), left.getScore()))
+                .takeWhile(st -> st.getScore() > bound)
+                .collect(Collectors.toList());
+    }
+}
