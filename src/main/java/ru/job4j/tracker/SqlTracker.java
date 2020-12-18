cfcdb3db1446d07f675f8df7f60476117b3626a3
reybos <andreybosiy@yandex.ru>
2020-12-18 21:42:41 +0300

    4.4. Liquibase. Интеграционные тесты. Proxy. [#404059]

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -9,6 +9,14 @@ import java.util.Properties;
 public class SqlTracker implements Store {
     private Connection cn;
 
+    public SqlTracker() {
+
+    }
+
+    public SqlTracker(Connection cn) {
+        this.cn = cn;
+    }
+
     public void init() {
         try (InputStream in = SqlTracker.class
                 .getClassLoader()
--- src/test/java/ru/job4j/tracker/SqlTrackerTest.java
+++ src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -1,77 +1,104 @@
 package ru.job4j.tracker;
 
-import org.junit.Before;
 import org.junit.Test;
 
-import java.sql.SQLException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
+import java.util.Properties;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
 
 public class SqlTrackerTest {
-    private Store tracker;
+    public Connection init() {
+        try (InputStream in = SqlTracker.class
+                .getClassLoader()
+                .getResourceAsStream("app.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            Class.forName(config.getProperty("driver-class-name"));
+            return DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
 
-    @Before
-    public void init() {
-        tracker = new SqlTracker();
-        tracker.init();
-    }
-
-    @Test
-    public void whenAdd() throws SQLException {
-        Item item = new Item(0);
-        tracker.add(item);
-        assertTrue(item.getId() > 0);
-        tracker.delete(item.getId());
-    }
-
-    @Test
-    public void whenDelete() throws SQLException {
-        Item item = new Item(0);
-        tracker.add(item);
-        assertTrue(tracker.delete(item.getId()));
-    }
-
-    @Test
-    public void whenFindAll() throws SQLException {
-        tracker.add(new Item(0, "item1"));
-        tracker.add(new Item(0, "item2"));
-        List<Item> rsl = tracker.findAll();
-        for (int i = 0; i < rsl.size(); i++) {
-            assertEquals(String.format("item%d", (i + 1)), rsl.get(i).getName());
-            tracker.delete(rsl.get(i).getId());
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
         }
     }
 
     @Test
-    public void whenFindById() throws SQLException {
-        Item item = new Item(0, "test");
-        tracker.add(item);
-        Item rsl = tracker.findById(item.getId());
-        assertEquals(rsl.getName(), item.getName());
-        tracker.delete(item.getId());
+    public void createItem() throws Exception {
+        try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
+            tracker.add(new Item("name"));
+            assertThat(tracker.findByName("name").size(), is(1));
+        }
     }
 
     @Test
-    public void whenReplace() throws SQLException {
-        Item item = new Item(0, "test");
-        tracker.add(item);
-        item.setName("item");
-        tracker.replace(item.getId(), item);
-        assertEquals("item", tracker.findById(item.getId()).getName());
-        tracker.delete(item.getId());
+    public void whenAdd() throws Exception {
+        try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
+            Item item = new Item(0);
+            tracker.add(item);
+            assertTrue(item.getId() > 0);
+        }
     }
 
     @Test
-    public void whenFindByName() throws SQLException {
-        tracker.add(new Item(0, "item"));
-        tracker.add(new Item(0, "item"));
-        List<Item> rsl = tracker.findByName("item");
-        assertEquals(2, rsl.size());
-        for (int i = 0; i < rsl.size(); i++) {
-            assertEquals("item", rsl.get(i).getName());
-            tracker.delete(rsl.get(i).getId());
+    public void whenDelete() throws Exception {
+        try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
+            Item item = new Item(0);
+            tracker.add(item);
+            assertTrue(tracker.delete(item.getId()));
+        }
+    }
+
+    @Test
+    public void whenFindAll() throws Exception {
+        try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
+            tracker.add(new Item(0, "item1"));
+            tracker.add(new Item(0, "item2"));
+            List<Item> rsl = tracker.findAll();
+            for (int i = 0; i < rsl.size(); i++) {
+                assertEquals(String.format("item%d", (i + 1)), rsl.get(i).getName());
+            }
+        }
+    }
+
+    @Test
+    public void whenFindById() throws Exception {
+        try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
+            Item item = new Item(0, "test");
+            tracker.add(item);
+            Item rsl = tracker.findById(item.getId());
+            assertEquals(rsl.getName(), item.getName());
+        }
+    }
+
+    @Test
+    public void whenReplace() throws Exception {
+        try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
+            Item item = new Item(0, "test");
+            tracker.add(item);
+            item.setName("item");
+            tracker.replace(item.getId(), item);
+            assertEquals("item", tracker.findById(item.getId()).getName());
+        }
+    }
+
+    @Test
+    public void whenFindByName() throws Exception {
+        try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
+            tracker.add(new Item(0, "item"));
+            tracker.add(new Item(0, "item"));
+            List<Item> rsl = tracker.findByName("item");
+            assertEquals(2, rsl.size());
+            for (int i = 0; i < rsl.size(); i++) {
+                assertEquals("item", rsl.get(i).getName());
+            }
         }
     }
 }
